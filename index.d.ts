c6058ce5939f9b195892be0085a2c7750872b688
Victor Trusov <v.trusov@artlogics.ru>
2021-07-10 14:59:21 +0300

    Added Switch to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,33 @@
 declare module "react-router-loading" {
-    import type { RouteProps } from "react-router-dom";
+    import type { RouteProps, SwitchProps, Switch } from "react-router-dom";
     // React router is a dependency of react-router-dom
     import type { OmitNative } from "react-router";
+
+    export class Switch extends React.Component<
+        SwitchProps
+        & OmitNative<T, keyof SwitchProps>
+        & {
+            loadingScreen?: React.Component,
+            maxLoadingTime?: number
+        },
+        any
+    > { }
 
     export class Route<
         T extends {} = {},
         Path extends string = string
     > extends React.Component<
-        RouteProps<Path> &
-            OmitNative<T, keyof RouteProps> & { loading?: boolean },
+        RouteProps<Path>
+        & OmitNative<T, keyof RouteProps>
+        & { loading?: boolean },
         any
     > {}
 
-    export const LoadingContext: React.Context<{ done(): void }>;
-    // Only for typings
-    export { Switch, SwitchProps } from "react-router-dom";
+    export const LoadingContext: React.Context<{
+        start(): void,
+        done(): void,
+        restart(): void
+    }>;
 
     export interface TopBarConfig {
         autoRun?: boolean;
